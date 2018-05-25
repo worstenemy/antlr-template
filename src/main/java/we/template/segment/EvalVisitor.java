31efb9d9edf6ac4522c0cb85e90a76e5a2f7b640
pxr <pxr@PengXinruideMacBook-Pro.local>
2018-05-26 00:02:31 +0800

    refractor code

--- src/main/java/we/template/segment/EvalVisitor.java
+++ src/main/java/we/template/segment/EvalVisitor.java
@@ -9,78 +9,78 @@ import we.template.antlr.TemplateParser;
 import java.util.List;
 
 public class EvalVisitor extends TemplateBaseVisitor<Object> {
-	/************************************* field and method *************************************/
+  /************************************* field and method *************************************/
 
-	@Override
-	public Object visitOSymbol(TemplateParser.OSymbolContext ctx) {
-		String symbol = ctx.IDENTIFIER().getText();
-		return RuntimeManager.arg(symbol);
-	}
+  @Override
+  public Object visitOSymbol(TemplateParser.OSymbolContext ctx) {
+    String symbol = ctx.IDENTIFIER().getText();
+    return RuntimeManager.arg(symbol);
+  }
 
-	@Override
-	public Object visitOArgCall(TemplateParser.OArgCallContext ctx) {
-		Object target = visit(ctx.tinyObject());
-		String methodName = ctx.IDENTIFIER().getText();
+  @Override
+  public Object visitOArgCall(TemplateParser.OArgCallContext ctx) {
+    Object target = visit(ctx.tinyObject());
+    String methodName = ctx.IDENTIFIER().getText();
 
-		List<TemplateParser.TinyExprContext> exprContexts = ctx.tinyExpr();
-		Object[] args = new Object[exprContexts.size()];
-		for (int i = 0; i < exprContexts.size(); ++i) {
-			args[i] = visit(exprContexts.get(i));
-		}
+    List<TemplateParser.TinyExprContext> exprContexts = ctx.tinyExpr();
+    Object[] args = new Object[exprContexts.size()];
+    for (int i = 0; i < exprContexts.size(); ++i) {
+      args[i] = visit(exprContexts.get(i));
+    }
 
-		return Reflection.invokeMethod(target, methodName, args);
-	}
+    return Reflection.invokeMethod(target, methodName, args);
+  }
 
-	@Override
-	public Object visitONonArgCall(TemplateParser.ONonArgCallContext ctx) {
-		Object target = visit(ctx.tinyObject());
-		String methodName = ctx.IDENTIFIER().getText();
+  @Override
+  public Object visitONonArgCall(TemplateParser.ONonArgCallContext ctx) {
+    Object target = visit(ctx.tinyObject());
+    String methodName = ctx.IDENTIFIER().getText();
 
-		return Reflection.invokeMethod(target, methodName);
-	}
+    return Reflection.invokeMethod(target, methodName);
+  }
 
-	@Override
-	public Object visitOAccess(TemplateParser.OAccessContext ctx) {
-		Object target = visit(ctx.tinyObject());
-		String fieldName = ctx.IDENTIFIER().getText();
+  @Override
+  public Object visitOAccess(TemplateParser.OAccessContext ctx) {
+    Object target = visit(ctx.tinyObject());
+    String fieldName = ctx.IDENTIFIER().getText();
 
-		return Reflection.invokeField(target, fieldName);
-	}
+    return Reflection.invokeField(target, fieldName);
+  }
 
-	/***************************************** function *****************************************/
-	@Override
-	public Object visitTinyCall(TemplateParser.TinyCallContext ctx) {
-		String symbol = ctx.IDENTIFIER().getText();
-		List<TemplateParser.TinyExprContext> contexts = ctx.tinyExpr();
-		Object[] args = new Object[contexts.size()];
-		for (int i = 0; i < contexts.size(); ++i) {
-			args[i] = visit(contexts.get(i));
-		}
-		Function function;
-		if (null != (function = RuntimeManager.function(symbol))) {
-			return function.call(args);
-		}
-		throw new RuntimeException("function not found");
-	}
+  /***************************************** function *****************************************/
+  @Override
+  public Object visitTinyCall(TemplateParser.TinyCallContext ctx) {
+    String symbol = ctx.IDENTIFIER().getText();
+    List<TemplateParser.TinyExprContext> contexts = ctx.tinyExpr();
+    Object[] args = new Object[contexts.size()];
+    for (int i = 0; i < contexts.size(); ++i) {
+      args[i] = visit(contexts.get(i));
+    }
+    Function function;
+    if (null != (function = RuntimeManager.function(symbol))) {
+      return function.call(args);
+    }
+    throw new RuntimeException("function not found " + symbol);
+  }
 
-	@Override
-	public Object visitECall(TemplateParser.ECallContext ctx) {
-		return visitTinyCall(ctx.tinyCall());
-	}
+  @Override
+  public Object visitECall(TemplateParser.ECallContext ctx) {
+    return visitTinyCall(ctx.tinyCall());
+  }
 
-	@Override
-	public Object visitEId(TemplateParser.EIdContext ctx) {
-		String symbol = ctx.IDENTIFIER().getText();
-		return RuntimeManager.arg(symbol);
-	}
+  @Override
+  public Object visitEId(TemplateParser.EIdContext ctx) {
+    String symbol = ctx.IDENTIFIER().getText();
+    return RuntimeManager.arg(symbol);
+  }
 
-	@Override
-	public Object visitEInt(TemplateParser.EIntContext ctx) {
-		return Integer.valueOf(ctx.INTEGER_VALUE().getText());
-	}
+  @Override
+  public Object visitEInt(TemplateParser.EIntContext ctx) {
+    return Integer.valueOf(ctx.INTEGER_VALUE().getText());
+  }
 
-	@Override
-	public Object visitEStr(TemplateParser.EStrContext ctx) {
-		return ctx.QUOTED_IDENTIFIER().getText();
-	}
+  @Override
+  public Object visitEStr(TemplateParser.EStrContext ctx) {
+    return ctx.QUOTED_IDENTIFIER().getText();
+  }
 }
