9e4baae4c8251c0a88bfea7860a4c5dc08d15854
pxr <pxr@PengXinruideMacBook-Pro.local>
2018-09-18 22:59:22 +0800

    add test

--- src/test/java/we/TemplateTest.java
+++ src/test/java/we/TemplateTest.java
@@ -1,12 +1,22 @@
 package we;
 
+import org.antlr.v4.runtime.BufferedTokenStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenSource;
+import org.antlr.v4.runtime.TokenStream;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import we.template.evaluator.ParseHelper;
+import we.template.evaluator.ParseResult;
+import we.template.evaluator.antlr.QueryLexer;
+import we.template.evaluator.antlr.QueryParser;
 import we.template.evaluator.expression.Expression;
 import we.template.evaluator.expression.ExpressionBuilder;
 import we.template.evaluator.function.impl.GlobalContext;
+
+import java.util.List;
 
 @RunWith(JUnit4.class)
 public class TemplateTest {
@@ -53,4 +63,20 @@ public class TemplateTest {
 		Expression e = ExpressionBuilder.build(template);
 		Assert.assertEquals("where 1 = 1 and 'abc'", e.evaluate());
 	}
+
+	@Test
+	public void test_token_1() {
+	  String template = "${a}";
+    ParseResult result = ParseHelper.parse("token",
+      template,
+      QueryParser::new,
+      QueryLexer::new,
+      QueryParser::query,
+      false);
+    BufferedTokenStream stream = (BufferedTokenStream) result.getStream();
+    List<Token> tokens = stream.getTokens();
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
 }
